d66aca0a26ce66dce481ff882999363d0535c3d4
tb-rules10 <tanishq.bhatt10@gmail.com>
2023-03-15 00:05:29 +0530

    minor changes

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import Request, FastAPI, Depends
-from EmotionDetector import EmotionDetector
-from EmotionDetector import EmotionGenre
+from Models import EmotionDetector
+from Models import EmotionGenre
 import pickle
 import json
 # from pprint import pprint
@@ -19,13 +19,14 @@ def read_root():
 @app.post("/api/input")
 async def preferences(req: Request):
     data:EmotionGenre = json.loads(await req.body())
-    # print(data)
+    print(data)
     return { "status": True, "msg": "Hi, I'm FastAPI" }
 
 
 @app.post("/api/predict")
 async def predict_Emotion(req: Request):
     data:EmotionGenre = json.loads(await req.body())
+    print(data)
     input =  data['text']
     res = classifier.predict([input])[0]
     return { "status": True, "msg": f'{input}   ->   {res}' }
